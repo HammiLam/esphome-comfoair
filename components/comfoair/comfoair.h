b4ef704b706bfbb84dc8663a233e73442fc85559
HammiLam <hvelst@gmail.com>
2026-05-06 17:12:58 +0200

    Update comfoair.h

--- components/comfoair/comfoair.h
+++ components/comfoair/comfoair.h
@@ -20,6 +20,20 @@ static const uint8_t COMFOAIR_MIN_SUPPORTED_TEMP = 12;
 static const uint8_t COMFOAIR_MAX_SUPPORTED_TEMP = 29;
 static const float COMFOAIR_SUPPORTED_TEMP_STEP = 0.5f;
 
+class ComfoAirComponent;
+
+class ComfoAirSizeSelect : public select::Select
+{
+public:
+  void set_parent(ComfoAirComponent *parent) { this->parent_ = parent; }
+
+protected:
+  void control(const std::string &value) override;
+
+private:
+  ComfoAirComponent *parent_{nullptr};
+};
+
 class ComfoAirComponent : public climate::Climate, public api::CustomAPIDevice, public PollingComponent, public uart::UARTDevice {
 public:
   // Poll every 600ms
